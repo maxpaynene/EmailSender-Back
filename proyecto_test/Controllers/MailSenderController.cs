0712e6c28b73f8d8f43cbca3a8d3db22f667c07b
maxpaynene <uletron1992@hotmail.com>
2023-08-12 13:50:33 -0400

    finish

---
+++ proyecto_test/Controllers/MailSenderController.cs
@@ -0,0 +1,56 @@
+﻿using System.Net;
+using System.Net.Mail;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EmailApi.Controllers
+{
+    [Route("api/email-sender")]
+    [ApiController]
+    public class EmailSenderController : ControllerBase
+    {
+        [HttpPost]
+        public async Task<IActionResult> SendEmail([FromForm] Models.EmailForm formData)
+        {
+            try
+            {
+                SmtpClient smtpClient = new SmtpClient("smtp.mailgun.org")
+                {
+                    Port = 587,
+                    // TODO: encripte password
+                    Credentials = new NetworkCredential("postmaster@sandboxc4f9463ef84a4f4fa8e1889658c5a0e5.mailgun.org", "895425a4e4c344d11895cc3dbddc0527-28e9457d-ecad2426"),
+                    EnableSsl = true,
+                };
+
+                MailMessage mailMessage = new MailMessage
+                {
+                    From = new MailAddress("brad@sandboxc4f9463ef84a4f4fa8e1889658c5a0e5.mailgun.org"),
+                    Subject = "Proyect sender mail",
+                    Body = $"Nombre: {formData.Name}\n Rut: {formData.Rut}\n Email: {formData.Email}",
+                    IsBodyHtml = false,
+
+                };
+
+                //TODO: find server smtp free that support attachment
+                /*if (formData.PdfFile != null && formData.PdfFile.Length > 0)
+                {
+                    using (var stream = new MemoryStream())
+                    {
+                        await formData.PdfFile.CopyToAsync(stream);
+                        var attachment = new Attachment(stream, formData.PdfFile.FileName, "application/pdf");
+                        mailMessage.Attachments.Add(attachment);
+                    }
+                }*/
+
+                mailMessage.To.Add(formData.Email);
+
+                await smtpClient.SendMailAsync(mailMessage);
+
+                return Ok("{status: 200}");
+            }
+            catch (Exception ex)
+            {
+                return BadRequest($"Error exception: {ex.Message}");
+            }
+        }
+    }
+}
---
+++ proyecto_test/Models/Email.cs
@@ -0,0 +1,9 @@
+﻿namespace Models;
+
+public class EmailForm
+{
+    public required string Name { get; set; }
+    public required string Email { get; set; }
+    public required string Rut { get; set; }
+    public required IFormFile PdfFile { get; set; }
+}
